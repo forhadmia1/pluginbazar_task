0987d83378c5bc8b1c3ba8b8c56d5e9740daabcb
forhadmia1 <forhad8613@gmail.com>
2022-08-25 01:20:21 +0600

    import data done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,8 +3,11 @@ import { BsFillPlusCircleFill } from 'react-icons/bs';
 import FormRow from '../components/FormRow';
 import { v4 as uuidv4 } from 'uuid';
 import { useNavigate } from 'react-router-dom'
+import { useCSVReader } from 'react-papaparse';
 
 const Home = ({ users, setUsers }) => {
+    const { CSVReader } = useCSVReader();
+    // const buttonRef = useRef(null)
     const [error, setError] = useState(true)
     const navigate = useNavigate()
     const [inputFields, setInputFields] = useState([{ id: uuidv4(), username: '', password: '', phone: '', email: '' }])
@@ -31,6 +34,11 @@ const Home = ({ users, setUsers }) => {
         }
     }
 
+    // const onfileLoad = (data) => {
+
+    // }
+
+
 
     return (
         <div className='mt-10 px-24'>
@@ -49,17 +57,50 @@ const Home = ({ users, setUsers }) => {
                 }
                 <div className='mt-5 flex items-center text-primary text-lg font-semibold'>
                     <BsFillPlusCircleFill />
-                    <button onClick={handleAddFields} className='ml-2'>Add More</button>
+                    <button onClick={(e) => handleAddFields(e)} className='ml-2'>Add More</button>
                 </div>
 
                 <div className='w-full h-[3px] bg-bg'></div>
                 <div className='flex justify-end mr-4 mt-4'>
-                    <input value={'Import'} type='file' className='text-primary font-bold' />
-                    <input type='file' value='Submit' className='bg-primary py-2 px-6 rounded' />
+                    <CSVReader
+                        onUploadAccepted={(results) => {
+                            const datas = results.data
+                            const title = results.data[0]
+                            datas.shift()
+                            const newData = datas.map(data => {
+                                const obj = {}
+                                title.forEach((d, index) => {
+                                    const key = d
+                                    const value = data[index];
+                                    obj[key] = value
+                                })
+                                return obj;
+                            })
+                            setUsers(newData);
+                            navigate('/user')
+                        }}
+                    >
+                        {({
+                            getRootProps,
+                            acceptedFile,
+                        }) => (
+                            <>
+                                <div >
+                                    <button type='button' className='text-primary font-bold mt-2 mr-3' {...getRootProps()}>
+                                        Browse file
+                                    </button>
+                                    <div >
+                                        {acceptedFile && acceptedFile.name}
+                                    </div>
+                                </div>
+                            </>
+                        )}
+                    </CSVReader>
+                    <input type='submit' value='Submit' className='bg-primary py-2 px-6 rounded' />
                 </div>
 
-            </form>
-        </div>
+            </form >
+        </div >
     );
 };
 
